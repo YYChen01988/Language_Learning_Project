284c9e5bc481306e57c45d89e4c28ae640dc12a7
yingying <chen.yingying.01988@gmail.com>
2018-09-07 11:03:59 +0100

    test server

---
+++ client/src/app.js
@@ -0,0 +1,14 @@
+<<<<<<< Updated upstream
+=======
+const Flashcard = require('./models/flashcard.js');
+const FlashcardView = require('./views/flashcard_view.js');
+const FlashcardGridView = require('./views/flashcard_grid_view');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+  // const wordsUrl = 'http://localhost:3000/api/words';
+  // const flashcard = new Flashcard(wordsUrl);
+  // flashcard.getData();
+});
+>>>>>>> Stashed changes
---
+++ client/src/models/flashcard.js
@@ -0,0 +1,14 @@
+// const Request = require('../helpers/request.js');
+// const PubSub = require('../helpers/pub_sub.js');
+//
+// const Flashcard = function () {
+//   this.request = new Request(this.url);
+// };
+
+// Flashcard.prototype.
+// getData = function(){
+//   this.request.get().then(() => {
+//
+//     // PubSub.publish('Flashcard:words_data_loaded', words);
+//   }).catch(console.error);
+// };
