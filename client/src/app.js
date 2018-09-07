0d8dd138b70ec7a9c8d00a88a9536cd2553497da
janeduncan <janeduncan@live.co.uk>
2018-09-07 14:54:24 +0100

    Finishes getData function in model

--- client/src/app.js
+++ client/src/app.js
@@ -5,14 +5,8 @@ const FlashcardGridView = require('./views/flashcard_grid_view');
 
 document.addEventListener('DOMContentLoaded', () => {
 
-
-
-  // const wordsUrl = 'http://localhost:3000/api/words';
-  // const flashcard = new Flashcard(wordsUrl);
-  // flashcard.getData();
-  // });
-
   const wordsUrl = 'http://localhost:3000/api/words';
   const flashcard = new Flashcard(wordsUrl);
+  flashcard.getData();
 
 });
