6db335ac1de1f239d01db6abb12a9ee89accf6ac
Craig Bowditch <craigbowditch@hotmail.com>
2018-09-09 15:28:36 +0100

    adds postWord method and bind event in model

--- client/src/models/flashcard.js
+++ client/src/models/flashcard.js
@@ -7,6 +7,14 @@ const Flashcard = function(url) {
   this.languages = [];
 };
 
+
+Flashcard.prototype.bindEvents = function(){
+
+  PubSub.subscribe('AddWordFormView:item-submitted', (event) => {
+    this.postWord(event.detail);
+  });
+}
+
 Flashcard.prototype.getData = function(){
   this.request.get()
   .then((languages) => {
@@ -32,6 +40,16 @@ Flashcard.prototype.publishByLanguage = function(languages){
     PubSub.publish("Flashcard:selected-language-and-answer", selectedLanguageWords);
     // console.log(selectedLanguageWords);
   });
+};
+
+Flashcard.prototype.postWord = function(word){
+  // console.log('what is the word:', word);
+  this.request.post(word)
+  .then((languages) => {
+    PubSub.publish('Languages:languages-data-ready', languages);
+    // console.log("languages is:", languages);
+  })
+  .catch(console.error);
 };
 
 module.exports = Flashcard;
