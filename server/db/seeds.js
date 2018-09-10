e8c96736f0024f6131c14b35648b8117db051cbd
Pim <pimgroeneveld92@gmail.com>
2018-09-10 13:16:43 +0100

    full seeds with audio per word

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -2,55 +2,145 @@ use languages;
 db.dropDatabase();
 
 db.words.insertMany([
-  {
-    English: "Thank you",
-    translation: {Chinese: "你好",
-                  German: "Danke",
-                  Dutch: "Bedankt"},
-    audio:{
-      Chinese:"audio/01.mp3",
-      German:"audio/01.mp3",
-      Dutch:"audio/01.mp3",
+    {
+      English: "Thank you",
+      translation: {Chinese: "你好",
+                    German: "Danke",
+                    Dutch: "Bedankt"},
+      audio:{
+        Chinese:"audio/01.mp3",
+        German:"audio/01.mp3",
+        Dutch:"audio/01.mp3",
+      }
+    },
+    {
+      English: "Hungry",
+      translation: {Chinese: "飢餓",
+                    German: "Hungrig",
+                    Dutch: "Hongerig"},
+      audio:{
+        Chinese:"audio/01.mp3",
+        German:"audio/01.mp3",
+        Dutch:"audio/01.mp3",
+      }
+    },
+    {
+      English: "Swim",
+      translation: {Chinese: "游泳",
+                    German: "Schwimmen",
+                    Dutch: "Zwemmen"},
+      audio:{
+        Chinese:"audio/01.mp3",
+        German:"audio/01.mp3",
+        Dutch:"audio/01.mp3",
+      }
+    },
+    {
+      English: "Please",
+      translation: {Chinese: "請",
+                    German: "Bitte",
+                    Dutch: "Alsjeblieft"},
+      audio:{
+        Chinese:"audio/01.mp3",
+        German:"audio/01.mp3",
+        Dutch:"audio/01.mp3",
+      }
+    },
+    {
+      English: "Beer",
+      translation: {Chinese: "啤酒",
+                    German: "Bier",
+                    Dutch: "Bier"},
+      audio:{
+        Chinese:"audio/01.mp3",
+        German:"audio/01.mp3",
+        Dutch:"audio/01.mp3",
+      }
+    },
+    {
+      English: "Wine",
+      translation: {Chinese: "紅酒",
+                    German: "Wein",
+                    Dutch: "Wijn"},
+      audio:{
+        Chinese:"audio/01.mp3",
+        German:"audio/01.mp3",
+        Dutch:"audio/01.mp3",
+      }
+    },
+    {
+      English: "Cycling",
+      translation: {Chinese: "循環",
+                    German: "Radfahren",
+                    Dutch: "Fietsen"},
+      audio:{
+        Chinese:"audio/01.mp3",
+        German:"audio/01.mp3",
+        Dutch:"audio/01.mp3",
+      }
+    },
+    {
+      English: "Good day",
+      translation: {Chinese: "美好的一天",
+                    German: "Schönen Tag",
+                    Dutch: "Goedendag"},
+      audio:{
+        Chinese:"audio/01.mp3",
+        German:"audio/01.mp3",
+        Dutch:"audio/01.mp3",
+      }
+    },
+    {
+      English: "Goodbye",
+      translation: {Chinese: "再見",
+                    German: "Auf Wiedersehen",
+                    Dutch: "Tot ziens"},
+      audio:{
+        Chinese:"audio/01.mp3",
+        German:"audio/01.mp3",
+        Dutch:"audio/01.mp3",
+      }
+    },
+    {
+      English: "Learning",
+      translation: {Chinese: "學習",
+                    German: "Lernen",
+                    Dutch: "Leren"},
+      audio:{
+        Chinese:"audio/01.mp3",
+        German:"audio/01.mp3",
+        Dutch:"audio/01.mp3",
+      }
     }
-  },
-  {
-    English: "Jobby",
-    translation: {Chinese: "你好",
-                  German: "Danke",
-                  Dutch: "Bedankt"},
-    audio:{
-      Chinese:"audio/01.mp3",
-      German:"audio/01.mp3",
-      Dutch:"audio/01.mp3",
-    }
-  }
-]);
-db.quiz.insertMany([
-  {"Chinese": [
-  {
-    question: "Hello",
-    answer: "Jobby",
-    possible: {one: "Wee Jobby",
-              two: "Big Jobby",
-              three: "Jobby",
-              four: "Job"}
-  }]},
-  {"German": [
-  {
-    question: "Hello",
-    answer: "Jobby",
-    possible: {one: "Wee Jobby",
-              two: "Big Jobby",
-              three: "Jobby",
-              four: "Job"}
-  }]},
-  {"Dutch": [
-  {
-    question: "Hello",
-    answer: "Jobby",
-    possible: {one: "Wee Jobby",
-              two: "Big Jobby",
-              three: "Jobby",
-              four: "Job"}
-  }]},
+  ]);
+  db.quiz.insertMany([
+    {"Chinese": [
+    {
+      question: "Hello",
+      answer: "Jobby",
+      possible: {one: "Wee Jobby",
+                two: "Big Jobby",
+                three: "Jobby",
+                four: "Job"}
+    }]},
+    {"German": [
+    {
+      question: "Hello",
+      answer: "Jobby",
+      possible: {one: "Wee Jobby",
+                two: "Big Jobby",
+                three: "Jobby",
+                four: "Job"}
+    }]},
+    {"Dutch": [
+    {
+      question: "Hello",
+      answer: "Jobby",
+      possible: {one: "Wee Jobby",
+                two: "Big Jobby",
+                three: "Jobby",
+                four: "Job"}
+    }]},
+  ]);
+
 ]);
